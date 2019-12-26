89d5671a558df5b385eb13fe7b421af4a5c6f260
likonion <likonion@outlook.com>
2019-12-26 09:50:53 +0800

    取消复制版权

--- dev/js/index.js
+++ dev/js/index.js
@@ -256,11 +256,11 @@ $(document).ready(function() {
             e.preventDefault();
         });
     }
-    $('.post-content').on('mouseup', function(e) {
-        var txt = window.getSelection();
-        if (txt.toString().length >= 30) {
-            setClipboardData(txt);
-        }
-    });
+    // $('.post-content').on('mouseup', function(e) {
+    //     var txt = window.getSelection();
+    //     if (txt.toString().length >= 30) {
+    //         setClipboardData(txt);
+    //     }
+    // });
 
 });
